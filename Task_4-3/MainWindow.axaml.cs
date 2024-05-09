07fd025e34651e075dc23239742c9dfe80bad77c
NenVan155 <i.nenov155@gmail.com>
2024-05-09 22:22:53 +0300

    done&

---
+++ Task_4-3/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Task_4_3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
